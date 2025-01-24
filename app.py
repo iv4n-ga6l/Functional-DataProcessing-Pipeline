d3b483644c6167698ada9eb7a8c4e94a8f870d6e
Ivan <apedoivan@gmail.com>
2025-01-24 13:39:08 +0100

    Fix debug config

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ from pycache_handler.handler import py_cache_handler
 
 
 app = Flask(__name__)
-app.config.from_object(config[os.getenv('FLASK_ENV', 'development')])
+app.config['DEBUG'] = config[os.getenv('FLASK_ENV', 'development')]
 app.config['UPLOAD_FOLDER'] = 'uploads'
 app.config['MAX_CONTENT_LENGTH'] = 16 * 1024 * 1024  # 16MB max file size
 
